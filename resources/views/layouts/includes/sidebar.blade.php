ffc334f6a94f4d080b3642098c44642d288af0a5
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-29 23:32:44 +0700

    install laravel dan integrasi template

---
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="d-flex justify-content-between">
+                <div class="logo">
+                    <a href="index.html">PRO.ID</a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item active ">
+                    <a href="index.html" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-stack"></i>
+                        <span>Materi</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="#">List Materi</a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+@include('layouts.includes.header')
+
+<body>
+    <div id="app">
+        @include('layouts.includes.sidebar')
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            <div class="page-heading">
+                <h3>Dashboard Admin</h3>
+            </div>
+            <div class="page-content">
+                @yield('content')
+            </div>
+
+            @include('layouts.includes.footer')
+        </div>
+    </div>
+    <script src="{{ asset('admin/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('admin/assets/vendors/apexcharts/apexcharts.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/pages/dashboard.js') }}"></script>
+
+    <script src="{{ asset('admin/assets/js/main.js') }}"></script>
+</body>
+
+</html>
